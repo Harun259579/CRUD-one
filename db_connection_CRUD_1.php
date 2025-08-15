44fa6d76c9c33fd1efb32b95fd4cbd47564ba0a8
Md.Harun <mdh97473@gmail.com>
2025-08-15 10:50:52 +0600

    Add files via upload

---
+++ db_connection_CRUD_1.php
@@ -0,0 +1,20 @@
+<?php
+
+$db_host='localhost';
+$db_user='root';
+$db_pass='';
+$db_name='CRUD_!';
+
+try
+{
+	$db_con = new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_pass);
+
+	$db_con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);//error dorar jonno
+}
+catch(PDOException $e)
+{
+	echo $e->getMessage();
+}
+
+
+?>
